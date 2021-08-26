8f0b06addbc9461bd77eae227033f0ec6041b096
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-08-26 14:56:48 -0300

    Phaser - Snake Seriously Just Die

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -83,12 +83,21 @@ export default class Snake {
     this.body[0].x = x;
     this.body[0].y = y;
 
+    //Death by going off screen
     if (
       this.body[0].x < 0 || 
       this.body[0].x >= this.scene.game.config.width || 
       this.body[0].y < 0 || 
       this.body[0].y >= this.scene.game.config.height
     ){
+      this.scene.scene.restart();
+    }
+
+    //Death by eating own tail - headpos === any of our tail positions
+    let tail = this.body.slice(1);
+    if(
+      tail.some(s => s.x === this.body[0].x && s.y === this.body[0].y)
+    ) {
       this.scene.scene.restart();
     }
   }
