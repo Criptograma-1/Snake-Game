ef71bb60429da22ec30b6fafc67d5bd853455205
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-08-26 13:03:41 -0300

    Phaser - Start the Snake

--- public/js/MainScene.js
+++ public/js/MainScene.js
@@ -1,13 +1,15 @@
+import Snake from './Snake.js'
+
 export default class MainScene extends Phaser.Scene {
   constructor(){
     super('MainScene');
   }
 
   create(){
- 
+    this.snake = new Snake(this);
   }
 
   update(time){
-    console.log('update');
+    this.snake.update(time);
   }
 }
