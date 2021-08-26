ebf3c2fcbb1b7bc9b7e59bee558f66518c9ed52e
Andresa Cristina <81261737+Criptograma-1@users.noreply.github.com>
2021-08-26 13:12:02 -0300

    Phaser - Patriot Snake

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -6,10 +6,16 @@ export default class Snake {
     this.direction = Phaser.Math.Vector2.LEFT;
     this.body = [];
     this.body.push(
-      this.scene.add.rectangle(100,100,16,16,0xff0000).setOrigin(0)
+      this.scene.add.rectangle(100,100,16,16,0x00ff00).setOrigin(0)
+      );
+    this.body.push(
+      this.scene.add.rectangle(0,0,16,16,0xffff00).setOrigin(0)
       );
     this.body.push(
       this.scene.add.rectangle(0,0,16,16,0x0000ff).setOrigin(0)
+      );
+    this.body.push(
+      this.scene.add.rectangle(0,0,16,16,0xffffff).setOrigin(0)
       );
     scene.input.keyboard.on('keydown', e => {
       this.keydown(e);
@@ -42,6 +48,10 @@ export default class Snake {
   }
 
   move() {
+    for (let index = this.body.length - 1; index > 0; index--){
+      this.body[index].x = this.body[index-1].x;
+      this.body[index].y = this.body[index-1].y;
+    }
     this.body[0].x += this.direction.x * 16;
     this.body[0].y += this.direction.y * 16;
   }
